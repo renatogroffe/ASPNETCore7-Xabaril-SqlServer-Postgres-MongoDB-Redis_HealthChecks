5e7599d06b2e0865b4388ce3152c52df74ff45ae
Renato Groffe <renato.groffe@yahoo.com.br>
2023-08-24 17:57:57 -0300

    Commit inicial
    Commit inicial

---
+++ WebAppMonitorBDs/Program.cs
@@ -0,0 +1,36 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configurando a verificação de disponibilidade de diferentes
+// serviços através de Health Checks
+builder.Services.AddHealthChecks()
+    .AddSqlServer(builder.Configuration.GetConnectionString("BaseContagemSqlServer")!,
+        name: "sqlserver", tags: new string[] { "db", "data", "sql" })
+    .AddNpgSql(builder.Configuration.GetConnectionString("BaseContagemPostgreSql")!,
+        name: "postgresql", tags: new string[] { "db", "data", "sql" })
+    .AddRedis(builder.Configuration.GetConnectionString("BaseContagemRedis")!,
+        name: "redis", tags: new string[] { "db", "data", "nosql" })
+    .AddMongoDb(builder.Configuration.GetConnectionString("BaseContagemMongoDB")!,
+        name: "mongodb", tags: new string[] { "db", "data", "nosql" });
+builder.Services.AddHealthChecksUI()
+    .AddSqlServerStorage(
+        builder.Configuration.GetConnectionString("BaseHealthChecks")!);
+
+var app = builder.Build();
+
+// Gera o endpoint que retornará os dados utilizados no dashboard
+app.UseHealthChecks("/healthchecks-data-ui", new HealthCheckOptions()
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+// Ativa o dashboard para a visualização da situação de cada Health Check
+app.UseHealthChecksUI(options =>
+{
+    options.UIPath = "/monitor";
+});
+
+app.Run();
